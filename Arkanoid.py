a9df7f88a5839f22b3dcc05e7b212661d13c4b7d
soneech <soneech@>
2020-01-03 14:18:39 +0300

    polnaya fignya

--- Arkanoid.py
+++ Arkanoid.py
@@ -8,12 +8,15 @@ screen = pygame.display.set_mode(size)
 screen.fill((99, 235, 235))
 
 running = True
-sprite_ball = pygame.sprite.Group()
-sprite_platform = pygame.sprite.Group()
+
+sprite_ball = pygame.sprite.Sprite()
+sprite_platform = pygame.sprite.Sprite()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(sprite_ball)
+all_sprites.add(sprite_platform)
 
 top_border = pygame.sprite.Group()
 bottom_border = pygame.sprite.Group()
-# horizontal_borders = pygame.sprite.Group()
 vertical_borders = pygame.sprite.Group()
 
 clock = pygame.time.Clock()
@@ -30,14 +33,16 @@ class Ball(pygame.sprite.Sprite):
     def __init__(self, ball):
         super().__init__(ball)
         self.ball_image = Ball.image
-        self.rect = self.ball_image.get_rect()
-        self.rect.x = random.randint(0, width)  # НАСТРОИТЬ ТАК, ЧТОБЫ ШАРИК СПАУНИЛСЯ НИЖЕ КУБИКОВ!!!
-        self.rect.y = random.randint(0, height - 40)
+        sprite_ball.image = self.ball_image
+        sprite_ball.rect = self.ball_image.get_rect()
+        sprite_ball.rect.x = random.randint(0, width)  # НАСТРОИТЬ ТАК, ЧТОБЫ ШАРИК СПАУНИЛСЯ НИЖЕ КУБИКОВ!!!
+        sprite_ball.rect.y = random.randint(0, height - 40)
+        self.rect = sprite_ball.rect
         self.vx = 2  # скорости по x и y
         self.vy = 2
 
     def update(self):
-        self.rect = self.rect.move(self.vx, self.vy)
+        sprite_ball.rect = sprite_ball.rect.move(self.vx, self.vy)
         if pygame.sprite.spritecollideany(self, sprite_platform):
             self.vy = -self.vy
         if pygame.sprite.spritecollideany(self, vertical_borders):
@@ -53,9 +58,14 @@ class Platform(pygame.sprite.Sprite):
     def __init__(self, platform):
         super().__init__(platform)
         self.platform_image = Platform.image
-        self.rect = self.platform_image.get_rect()
-        self.rect.x = width // 2 - self.rect[2] // 2
-        self.rect.y = height - 20
+        sprite_platform.image = self.platform_image
+        sprite_platform.rect = self.platform_image.get_rect()
+        sprite_platform.rect.x = width // 2 - sprite_platform.rect[2] // 2
+        sprite_platform.rect.y = height - 20
+        self.rect = sprite_platform.rect
+        # self.rect = self.platform_image.get_rect()
+        # self.rect.x = width // 2 - self.rect[2] // 2
+        # self.rect.y = height - 20
 
     def move(self):
         pass
@@ -64,7 +74,7 @@ class Platform(pygame.sprite.Sprite):
 class Border(pygame.sprite.Sprite):
     # строго вертикальный или строго горизонтальный отрезок
     def __init__(self, x1, y1, x2, y2, pos):
-        super().__init__(sprite_ball)
+        super().__init__(all_sprites)
         if pos == 'left' or pos == 'right':  # вертикальная стенка
             self.add(vertical_borders)
             self.image = pygame.Surface([1, y2 - y1])
@@ -84,8 +94,8 @@ Border(5, height - 5, width - 5, height - 5, 'bottom')
 Border(5, 5, 5, height - 5, 'left')
 Border(width - 5, 5, width - 5, height - 5, 'right')
 
-ball = Ball(sprite_ball)  # ДОРАБОТАТЬ!!!!!!!!!!
-platform = Platform(sprite_platform)
+ball = Ball(all_sprites)  # ДОРАБОТАТЬ!!!!!!!!!!
+platform = Platform(all_sprites)
 
 while running:
     for event in pygame.event.get():
@@ -93,7 +103,7 @@ while running:
             running = False
     clock.tick(60)  # ДОРАБОТАТЬ!!!!!!!!!!
     screen.fill((99, 235, 235))
-    sprite_ball.draw(screen)
+    all_sprites.draw(screen)
     sprite_ball.update()
     pygame.display.flip()
 
